20a2576a23d90f2450975ec76c1d2ffb1735154a
Sebastian Ljungberg <sebastian.ljungberg@widespace.com>
2013-10-03 00:15:20 +0200

    Added all old classes.
    Still a couple of errors though that needs to be handled!

---
+++ src/com/varnoss/Station.java
@@ -0,0 +1,5 @@
+package com.varnoss;
+
+public class Station {
+
+}
---
+++ src/com/varnoss/rest/DataServlet.java
@@ -0,0 +1,65 @@
+package com.varnoss.rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+
+import com.varnoss.Station;
+
+@Path("/api")
+public class DataServlet {
+	DBManager manager = new DBManager();
+
+	@POST
+	@Path("/warning")
+	@Consumes("application/json")
+	public Response postWarning(Warning war) {
+		manager.insertWarning(war);
+		return Response.ok(200).build();
+	}
+
+	@GET
+	@Path("/warning/{id}")
+	public Response getWarning(@QueryParam("id") String id) throws Exception {
+		Warning war = new Warning();
+		war = manager.getWarning(id);
+		return Response.ok(200).entity(war).build();
+	}
+
+	@GET
+	@Path("/warnings")
+	public Response getWarnings(
+			@DefaultValue("5") @QueryParam("amount") int amount, @QueryParam("stations") String s)
+			throws Exception {
+		String[] _stations = parseStations(s);
+		List<Warning> wars = new ArrayList<Warning>();
+		wars = manager.getWarnings(amount, _stations);
+		return Response.ok(200).entity(wars).build();
+	}
+
+	@GET
+	@Path("/stations")
+	public Response getStations(@QueryParam("name") String name,
+			@QueryParam("town") String town, @QueryParam("type") String type,
+			@QueryParam("line") String line) throws Exception {
+		List<Station> _stations = new ArrayList<Station>();
+		_stations = manager.getStations(name, town, type, line);
+		return Response.ok(200).entity(_stations).build();
+	}
+	
+	private String[] parseStations(String stations){
+		String[] _stations = stations.split(",");
+		return _stations;
+	}
+
+}
