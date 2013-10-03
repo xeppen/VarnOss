cdd4251012e040e4c676e42f75ed44d8ee3e5702
Sebastian Ljungberg <sebastian.ljungberg@widespace.com>
2013-10-03 11:51:10 +0200

    Fixed so the initial commit is working as expected.

--- src/com/varnoss/Station.java
+++ src/com/varnoss/Station.java
@@ -1,5 +1,35 @@
 package com.varnoss;
 
 public class Station {
-
+	String station = "";
+	String town = "";
+	String type = "";
+	String line = "";
+	
+	public String getStation() {
+		return station;
+	}
+	public String getTown() {
+		return town;
+	}
+	public void setStation(String station) {
+		this.station = station;
+	}
+	public void setTown(String town) {
+		this.town = town;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getLine() {
+		return line;
+	}
+	public void setLine(String line) {
+		this.line = line;
+	}
+	
+	
 }
--- src/com/varnoss/rest/DataServlet.java
+++ src/com/varnoss/rest/DataServlet.java
@@ -1,20 +1,18 @@
 package com.varnoss.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.MediaType;
-
-import com.google.gson.Gson;
-
 import com.varnoss.Station;
+import com.varnoss.Warning;
+import com.varnoss.sql.DBManager;
 
 @Path("/api")
 public class DataServlet {
